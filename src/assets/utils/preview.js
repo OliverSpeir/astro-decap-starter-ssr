432004bbe3c15ab058e6c805381ecc6b4f93052c
Oliver Speir <oliverspeir9@gmail.com>
2023-11-30 21:39:17 -0700

    add prose class to preview so typography styles apply

--- src/assets/utils/preview.js
+++ src/assets/utils/preview.js
@@ -1,13 +1,12 @@
 /* eslint-disable */
 var CustomPreview = createClass({
-	render: function () {
-		var entry = this.props.entry;
+    render: function () {
+        var entry = this.props.entry;
 
-		// Render only the body
-		return h("div", {}, this.props.widgetFor("body"));
-	},
+        // Add 'prose' class to the div for the tailwind typography styles
+        return h("div", { className: "prose" }, this.props.widgetFor("body"));
+    },
 });
 
 // Register the custom preview template for a specific collection
 CMS.registerPreviewTemplate("blog", CustomPreview);
-console.log("preview styles");
